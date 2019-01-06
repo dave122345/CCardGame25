1c0c73cf7c7faedda0a3e56f98ee2f18cf9020a8
David Nolan <davidnolan458@gmail.com>
2019-01-06 14:10:45 +0000

    BAckUp
    Just refreshing it

--- cardGame25/cardGame25/Source.c
+++ cardGame25/cardGame25/Source.c
@@ -8,7 +8,7 @@
 #define SUITS 4
 
 //Arrays
-char card[CARDS] = {'2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A','2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A','2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A','2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A' };
+char card[CARDS] = {'2', '3', '4', '5', '6', '7', '8', '9', 'X', 'J', 'Q', 'K','A','2', '3', '4', '5', '6', '7', '8', '9', 'X', 'J', 'Q', 'K', 'A','2', '3', '4', '5', '6', '7', '8', '9', 'X', 'J', 'Q', 'K', 'A','2', '3', '4', '5', '6', '7', '8', '9', 'X', 'J', 'Q', 'K', 'A' };
 char suit[SUITS] = { 'C', 'D', 'H', 'S' };
 
 
@@ -18,14 +18,14 @@ void main()
 	int i, j, rs, rc;
 	int players;
 	int cardCount = 5;
+	int p1Score, p2Score;
 	srand(time(NULL));
-	printf("Welcome to 25! the suit types are as follows: Diamond(D), Hearts(H), Spades(S) and clubs(C) with a Trump(T) Card. when you have entered the number of players press enter to proceed. Enjoy!\n");
-
-
+	printf("Welcome to 25! the suit types are as follows: Diamond(D), Hearts(H), Spades(S) and clubs(C) with a Trump(T) Card.\n"); 
+	printf("Format of play: when asked type the card you have as '2D' e.g. 5 of H is 5H\n");
+	printf("When you have entered the number of players press enter to proceed.Enjoy!\n");
+	
 	//Player ask
-	
-	
-	printf("How many players are there? (MIN 2)\n");
+	printf("How many players are there? (MAX 2)\n");
 	scanf("%d", &players);
 	
 	/*
@@ -44,43 +44,70 @@ void main()
 			rs = (rand() % 3) + 1;
 			rc = (rand() % 51) + 1;
 
-			/*if (suit[rs] == rs)//repeat suit test
-			{
-				printf("");
-			}//end
+				if(card[rc] == 0)
+				{
+					rs = (rand() % 3) + 1;
+					rc = (rand() % 50) + 1;
+					//printf("DEBUG RRRPlayer No. %d: Card number %d is %c of %c \n", i, j, card[rc], suit[rs]);
+					printf("Player No. %d: Card number %d is %c of %c \n", i, j, card[rc], suit[rs]);
+				}
+				else if (card[rc] == ' ')
+				{
+					rs = (rand() % 3) + 1;
+					rc = (rand() % 50) + 1;
+					//printf("DEBUG RRRPlayer No. %d: Card number %d is %c of %c \n", i, j, card[rc], suit[rs]);
+					printf("Player No. %d: Card number %d is %c of %c \n", i, j, card[rc], suit[rs]);
+				}
+				else
+				{
 
-			else if (card[rc] == rc)//repeat card test
-			{
-				printf("");
-			}//end*/
-			if(card[rc] == 0)
-			{
-				printf("");
-			}
-			else
-			{
-
-				printf("player No. %d: Card number %d is %c of %c \n", i, j, card[rc], suit[rs]);
+					printf("Player No. %d: Card number %d is %c of %c \n", i, j, card[rc], suit[rs]);
 				
-			}
+				}
+
 			card[rc] = 0;
 
 		}//End of card selection
 		printf("\n");
 
-		if (i == players)//Trump card
+			if (i == players)//Trump card
 
-		{
-			rs = (rand() % 3) + 1;
-			rc = (rand() % 12) + 1;
+			{
+				rs = (rand() % 3) + 1;
+				rc = (rand() % 12) + 1;
 			
-				
-
-			printf("Trump card is %c of %c \n", card[rc], suit[rs]);
-		}//End of trump
+			if (card[rc] == 0)//CHECK
+			{
+				rs = (rand() % 3) + 1;
+				rc = (rand() % 51) + 1;
+				//printf("DEBUG RRRTrump card is %c of %c \n", card[rc], suit[rs]);
+				printf("Trump card is %c of %c \n", card[rc], suit[rs]);
+			}
+			else if (card[rc] == " ")
+			{
+				rs = (rand() % 3) + 1;
+				rc = (rand() % 51) + 1;
+				//printf("DEBUG RRRTrump card is %c of %c \n", card[rc], suit[rs]);
+				printf("Trump card is %c of %c \n", card[rc], suit[rs]);
+			}
+			else
+			{
+				printf("Trump card is %c of %c \n", card[rc], suit[rs]);
+			}
+			}//End of trump
 		printf("\n");
 
 	}//End of dealing
+
+
+
+	//Start of gameplay
+		while (p1Score != 25 || p2Score != 25)
+		{
+			printf("");
+
+
+		}
 
 }//end of main
 	
